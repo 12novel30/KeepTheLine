5ca39313cac8eb563fe80e9e74e30adf5e0843ef
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-26 17:02:37 +0900

    Feat: check if application runs

--- src/main/java/com/fastcampus/keeptheline/KeepTheLineApplication.java
+++ src/main/java/com/fastcampus/keeptheline/KeepTheLineApplication.java
@@ -8,6 +8,7 @@ public class KeepTheLineApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KeepTheLineApplication.class, args);
+		System.out.println("fight!");
 	}
 
 }
