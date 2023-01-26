9d4d8da15a96dff7e2a167da6ade411072ae02e3
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-26 17:01:31 +0900

    Feat: project setting & gitignore

---
+++ src/main/java/com/fastcampus/keeptheline/KeepTheLineApplication.java
@@ -0,0 +1,13 @@
+package com.fastcampus.keeptheline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepTheLineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepTheLineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fastcampus/keeptheline/KeepTheLineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastcampus.keeptheline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepTheLineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
