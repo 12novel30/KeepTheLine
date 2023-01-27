1cf62a611f760852081703e159c6ff17d77db4a1
12novel30 <79260472+12novel30@users.noreply.github.com>
2023-01-27 14:45:35 +0900

    Add #2 - view controller
    5가지 view controller 작성

---
+++ src/main/java/com/fastcampus/keeptheline/controller/AuthController.java
@@ -0,0 +1,17 @@
+package com.fastcampus.keeptheline.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AuthController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @GetMapping("sign-up")
+    public String signUp(){
+        return "sign-up";
+    }
+}
--- src/main/java/com/fastcampus/keeptheline/controller/BaseController.java
+++ src/main/java/com/fastcampus/keeptheline/controller/BaseController.java
@@ -1,13 +1,24 @@
 package com.fastcampus.keeptheline.controller;
 
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class BaseController {
+public class BaseController implements ErrorController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/root")
+    public String root(){
         return "index";
+        // before using thymeleaf -> .html in static
+        // after thymeleaf
+        // -> index.html in /templates
+        // & 기본 페이지는 index.html (얘가 어디에 있든) 로 보여준다.
+    }
+
+    @RequestMapping("/error")
+    public String error(){
+        return "error";
     }
 }
